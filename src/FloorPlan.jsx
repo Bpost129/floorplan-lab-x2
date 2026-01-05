bf5b7900802ec0412a10aa166b7518b3d2dd2ef8
Bpost129 <Bpost129@yahoo.com>
2026-01-04 19:07:00 -0500

    import floorplan to app, import and add components to floorplan

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,7 +1,18 @@
+import Kitchen from "./Kitchen"
+import LivingRoom from "./LivingRoom"
+import Bedroom from "./Bedroom"
+import Bath from "./Bath"
+
 const FloorPlan = () => {
   return (
     <>
-    
+      <LivingRoom />
+      <Bath />
+      <Kitchen />
+      <Bedroom />
+      <Bedroom />
+      <Bath />
+      <Bedroom />
     </>
   )
 }
